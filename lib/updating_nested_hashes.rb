5d9895d1cdb494fafea4d6ee4bae1150d745ca8a
Mark G <markghaida@gmail.com>
2020-10-28 02:41:23 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -37,7 +37,6 @@ def add_characters
          :patriarch => {name: "Lord Montague", age: "53"},
          :matriarch => {name: "Lady Montague", age: "54"},
          :hero => {name: "Romeo", age: "15", status: "alive"},
-         :additional_characters => [1,2],
          :hero_friends => [
             {name: "Benvolio", age: "17", attitude: "worried"},
             {name: "Mercutio", age: "18", attitude: "hot-headed"}]
@@ -46,7 +45,6 @@ def add_characters
          :patriarch => {name: "Lord Capulet", age: "50"},
          :matriarch => {name: "Lady Capulet", age: "51"},
          :heroine => {name: "Juliet", age: "15", status: "alive"},
-         :additional_characters => [3,4],
          :heroine_friends => [
          {name: "Steven", age: "30", attitude: "confused"},
          {name: "Nurse", age: "44", attitude: "worried"}]
