7ea32c07f72c688ca1f8b9a9bbb977cf26a8a720
Mark G <markghaida@gmail.com>
2020-10-28 02:31:30 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -48,7 +48,8 @@ def add_characters
          :heroine_friends => [
          {name: "Steven", age: "30", attitude: "confused"},
          {name: "Nurse", age: "44", attitude: "worried"}
-         ]
+         ],
+         :additional_characters => []
       }
    }
 
