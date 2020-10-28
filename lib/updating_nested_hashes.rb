528696393c5d376111796c8166ec92d5dd665c57
Mark G <markghaida@gmail.com>
2020-10-28 02:34:30 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -17,7 +17,7 @@ def update_status
          {name: "Steven", age: "30", attitude: "confused"},
          {name: "Nurse", age: "44", attitude: "worried"}
          ],
-          :additional_characters => []
+          
       }
    }
 
